c4b29943ce5612946dab9c50df38d8ccaa0b9ece
Martin Borg <Martin.d@live.se>
2021-05-27 14:20:28 +0200

    fetching courses from database

--- app/wall_e/routes.py
+++ app/wall_e/routes.py
@@ -3,9 +3,10 @@ Contains routes for main purpose of app
 """
 from flask import current_app
 from app.wall_e import bp
-from app.wall_e.models import courses, assignments, submissions
 from app import db
-from app.models import Submission
+from app.models import Submission, Course
+from app.settings.api_config import API_KEY, API_URL
+from app.wall_e.models.canvas_api import Canvas, Grader
 
 @bp.before_request
 def before_request():
@@ -18,31 +19,39 @@ def before_request():
 
 
 @bp.route('/wall-e/fetch-submissions', methods=['GET', 'POST'])
-def index():
+def fetch():
     """
     Route for fetching gradable submissions
-
-    TODO: Vad skall man göra om submission redan finns?
-      (WHERE assignment_id=assignment_id AND user_id=user_id)
-          Lägg inte till den i databasen då den inväntar rättning?
     """
-    course_id = 2508
+    active_courses = Course.query.filter_by(active=1)
 
-    students = courses.users_and_acronyms(course_id=course_id)
-    subs = submissions.gradeable_submissions(course_id=course_id)
+    for c in active_courses:
+        canvas = Canvas(API_URL, API_KEY, course_id=c.id)
+        students = canvas.users_and_acronyms()
+        subs = canvas.get_gradeable_submissions()
 
-    for sub in subs:
-        assignment_id = sub["assignment_id"]
-        kmom = assignments.get_assignment_name_by_id(assignment_id=assignment_id, course_id=course_id)
+        for sub in subs:
+            assignment_id = sub["assignment_id"]
+            user_id = sub["user_id"]
 
-        user_id = sub["user_id"]
-        user_acronym = students[user_id]
+            exists = Submission.query.filter_by(
+                assignment_id=assignment_id, workflow_state='submitted',
+                user_id=user_id
+            ).count()
 
-        s = Submission(assignment_id=assignment_id, kmom=kmom, user_id=user_id, user_acronym=user_acronym, course_id=course_id)
-        db.session.add(s)
-        db.session.commit()
+            if exists:
+                continue
 
-    return { "subs": subs }
+            user_acronym = students[user_id]
+            kmom = canvas.get_assignment_name_by_id(assignment_id=assignment_id)
+            s = Submission(
+                assignment_id=assignment_id, kmom=kmom, user_id=user_id,
+                user_acronym=user_acronym, course_id=c.id)
+
+            db.session.add(s)
+            db.session.commit()
+
+    return {}
 
 
 
@@ -51,14 +60,12 @@ def grade():
     """
     Route for grading students
     """
-
+    grader = Grader(API_URL, API_KEY)
     graded_submissions = Submission.query.filter_by(workflow_state="pending_review")
-    for sub in graded_submissions:
-        submissions.grade_submission(
-            sub=sub
-        )
 
-        sub.workflow_state="graded"
+    for sub in graded_submissions:
+        grader.grade_submission(sub)
+        sub.workflow_state = "graded"
         db.session.commit()
 
-    return "Klar!"
+    return {}
