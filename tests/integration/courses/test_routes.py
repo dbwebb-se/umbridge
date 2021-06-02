c32f788118e8d03d18bf0191cdd1a274c383efaf
mabn17 <martin.d@live.se>
2021-06-02 14:27:53 +0200

    added tests for post and delete

--- tests/integration/courses/test_routes.py
+++ tests/integration/courses/test_routes.py
@@ -3,10 +3,12 @@ Contains tests for the app.auth module
 """
 
 # pylint: disable=unused-argument, disable=protected-access, disable=redefined-outer-name
+import json
 from werkzeug.datastructures import Headers
 import pytest
 from app.models import User, Course
 from app import db
+from app.courses.routes import get_course
 
 
 valid_header = Headers([('Authorization', "Basic ZGJ3ZWJiOnN1cGVyLXNlY3JldA==")])
@@ -50,13 +52,120 @@ def course1_data():
 
 
 
+def test_prepare():
+    """ Tests the Setup """
+    c = Course.query.filter_by(id=1).first()
+    c2 = Course.query.filter_by(id=99).first()
+    c3 = Course.query.filter_by(id=55).first()
+    count = db.session.query(Course).count()
+
+    assert str(c) == "<Course 1, python, Active: False>"
+    assert str(c2) == "<Course 99, oopython, Active: True>"
+    assert str(c3) == "<Course 55, webapp, Active: True>"
+    assert count == 3
+
+
+def test_get_course():
+    """  Tests a helper function """
+    valid_course = get_course({ 'id': 55 })
+    assert str(valid_course) == "<Course 55, webapp, Active: True>"
+
+    valid_data = get_course({ 'id': 50 })
+    assert valid_data is None
+
+    empty_data = get_course({})
+    assert empty_data is False
+
+    invalid_data = get_course({ 'name': 'python' })
+    assert invalid_data is False
+
 #################  GET  ###################################
+
+def test_get_courses_no_header(client):
+    """
+    Trys to get courses with no header
+    """
+    response = client.get('/courses')
+
+    assert response.status_code == 401
+    assert b"No Authorization token provided" in response.data
+
+
+def test_get_courses_invalid_header(client):
+    """
+    Trys to get courses with a non existing user
+    """
+    response = client.get('/courses', headers=non_exsiting_user_header)
+
+    assert response.status_code == 401
+    assert b"Invalid username or password" in response.data
+
+
+def test_get_courses_invalid_user(client):
+    """
+    Trys to get courses with a non existing user
+    """
+    response = client.get('/courses', headers=non_exsiting_user_header)
+
+    assert response.status_code == 401
+    assert b"Invalid username or password" in response.data
+
+
+def test_get_all_courses(client):
+    """
+    Trys to get all courses with am existing user
+    """
+    response = client.get('/courses', headers=valid_header)
+    r = json.loads(response.data)
+
+    assert response.status_code == 200
+    assert len(r['courses']) == 3
+    assert r['courses'][0] == { 'id': 55, 'name': 'webapp', 'active': 1 }
+    assert r['courses'][1] == { 'id': 99, 'name': 'oopython', 'active': 1 }
+    assert r['courses'][2] == { 'id': 1, 'name': 'python', 'active': 0 }
+
+
+def test_get_all_by_query_param(client):
+    """
+    Trys to get active courses
+    """
+    response = client.get('/courses?active=1', headers=valid_header)
+    r = json.loads(response.data)
+
+    assert response.status_code == 200
+    assert len(r['courses']) == 2
+    assert r['courses'][0] == { 'id': 55, 'name': 'webapp', 'active': 1 }
+    assert r['courses'][1] == { 'id': 99, 'name': 'oopython', 'active': 1 }
+
+
+def test_get_all_by_multiple_query_params(client):
+    """
+    Trys to get all active courses with the name oopython
+    """
+    response = client.get('/courses?active=1&name=oopython', headers=valid_header)
+    r = json.loads(response.data)
+
+    assert response.status_code == 200
+    assert len(r['courses']) == 1
+    assert r['courses'][0] == { 'id': 99, 'name': 'oopython', 'active': 1 }
+
+
+def test_get_all_by_invalid_query_params(client):
+    """
+    Trys to get all courses by using invalid params
+    """
+    response = client.get('/courses?active=1&user_id=2', headers=valid_header)
+    r = json.loads(response.data)
+
+    assert response.status_code == 400
+    assert r == { 'message': 'One or more parameter(s) does not exist' }
+
 
 #################  POST ###################################
 
 def test_add_course_no_header(course1_data, client):
     """
-    Trys to add course with a no header
+    Trys to add course with no header
     """
     response = client.post('/courses', data=course1_data)
 
@@ -139,4 +248,169 @@ def test_add_course_without_id(client):
 
 #################  PUT     ###################################
 
+def test_edit_course_no_header(course1_data, client):
+    """
+    Trys to edit course with no header
+    """
+    response = client.put('/courses', data=course1_data)
+    assert response.status_code == 401
+    assert b"No Authorization token provided" in response.data
+
+
+
+def test_edit_course_no_valid_user(course1_data, client):
+    """
+    Trys to edit a course with valid header but non existing user
+    """
+    response = client.put('/courses', data=course1_data, headers=non_exsiting_user_header)
+    assert response.status_code == 401
+    assert b"Invalid username or password" in response.data
+
+
+def test_edit_course_valid_active_only(client):
+    """
+    Trys a valid response and updates its active attribute.
+    """
+    data = { 'id': 1, 'name': 'python', 'active': 1 }
+    before = Course.query.filter_by(id=data['id']).first().serialize
+
+    response = client.put('/courses', data=data, headers=valid_header)
+
+    assert response.status_code == 200
+
+    res_json = json.loads(response.data)['course']
+    after = Course.query.filter_by(id=data['id']).first()
+
+    assert res_json == after.serialize
+    assert after.id == before['id']
+    assert after.name == before['name']
+    assert after.active != before['active']
+    assert after.active == 1
+
+
+
+def test_edit_course_valid_name_only(client):
+    """
+    Trys a valid response and updates its name attribute.
+    """
+    data = { 'id': 1, 'name': 'oophp', 'active': 0 }
+    before = Course.query.filter_by(id=data['id']).first().serialize
+
+    response = client.put('/courses', data=data, headers=valid_header)
+
+    assert response.status_code == 200
+
+    res_json = json.loads(response.data)['course']
+    after = Course.query.filter_by(id=data['id']).first()
+
+    assert res_json == after.serialize
+    assert after.id == before['id']
+    assert after.name != before['name']
+    assert after.active == before['active']
+    assert after.name == 'oophp'
+
+
+
+def test_edit_course_valid_both_name_and_active(client):
+    """
+    Trys a valid response and updates both its name and active attribute.
+    """
+    data = { 'id': 1, 'name': 'oophp', 'active': 1 }
+    before = Course.query.filter_by(id=data['id']).first().serialize
+
+    response = client.put('/courses', data=data, headers=valid_header)
+
+    assert response.status_code == 200
+
+    res_json = json.loads(response.data)['course']
+    after = Course.query.filter_by(id=data['id']).first()
+
+    assert res_json == after.serialize
+    assert after.id == before['id']
+    assert after.name != before['name']
+    assert after.active != before['active']
+    assert after.name == 'oophp'
+    assert after.active == 1
+
+
+def test_edit_course_with_no_data(client):
+    """
+    Trys to edit a course with no data provided
+    """
+    data = {}
+    response = client.put('/courses', data=data, headers=valid_header)
+    assert response.status_code == 400
+    assert b"Course not found" in response.data
+
+
+def test_edit_course_without_id(client):
+    """
+    Trys to edit a course with no id
+    """
+    data = { 'name': 'python', 'active': 0 }
+    response = client.put('/courses', data=data, headers=valid_header)
+    assert response.status_code == 400
+    assert b"Course not found" in response.data
+
+
+
 #################  DELETE  ###################################
+
+def test_delete_course_no_header(course1_data, client):
+    """
+    Trys to delete course with no header
+    """
+    response = client.put('/courses', data=course1_data)
+    assert response.status_code == 401
+    assert b"No Authorization token provided" in response.data
+
+
+def test_delete_course_no_valid_user(course1_data, client):
+    """
+    Trys to delete a course with valid header but non existing user
+    """
+    response = client.put('/courses', data=course1_data, headers=non_exsiting_user_header)
+    assert response.status_code == 401
+    assert b"Invalid username or password" in response.data
+
+
+def test_delete_course_with_no_data(client):
+    """
+    Trys to delete a course with no data provided
+    """
+    data = {}
+    response = client.delete('/courses', data=data, headers=valid_header)
+    assert response.status_code == 400
+    assert b"Course not found" in response.data
+
+
+def test_delete_course_without_id(client):
+    """
+    Trys to delete a course with no id
+    """
+    data = { 'name': 'python', 'active': 0 }
+    response = client.put('/courses', data=data, headers=valid_header)
+    assert response.status_code == 400
+    assert b"Course not found" in response.data
+
+
+def test_delete_course_valid(client):
+    """
+    Trys a valid response and updates both its name and active attribute.
+    """
+    data = { 'id': 1 }
+    exists = Course.query.filter_by(id=data['id']).first()
+    count = db.session.query(Course).count()
+
+    assert exists is not None
+    assert count == 3
+
+    response = client.delete('/courses', data=data, headers=valid_header)
+
+    after_delete = Course.query.filter_by(id=data['id']).first()
+    count_after = db.session.query(Course).count()
+    assert after_delete is None
+    assert count_after == 2
+
+    assert response.status_code == 200
+    assert b"<Course 1, python, Active: False> has been deleted" in response.data
