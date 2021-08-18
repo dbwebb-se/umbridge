91dbb88443a18ae25004f49d4bd726a48434c34f
Andreas Arnesson <andreas_osby@hotmail.com>
2021-08-18 13:51:12 +0200

    Now downloads zip from comment, unpacks and redirects to browse

--- app/results/routes.py
+++ app/results/routes.py
@@ -3,8 +3,10 @@ Contains routes for main purpose of app
 """
 
 import os
+import shutil
+import requests
 from ansi2html import Ansi2HTMLConverter
-from flask import render_template, abort, request
+from flask import render_template, abort, request, current_app, redirect
 from app.results import bp
 from app.models import Submission
 from app.settings.settings import APP_BASE_PATH
@@ -38,19 +40,49 @@ def get_log(log_id):
 
 
 
+@bp.route('/results/inspect/<id_>/<uuid_>')
+def download_zip(id_, uuid_):
+    """
+    Displays a file tree
+    """
+    DL_URL = f"https://bth.instructure.com/files/{id_}/download?download_frd=1&verifier={uuid_}"
+    current_app.logger.debug(f"Downloading {DL_URL}")
+    TEMP_DIR = APP_BASE_PATH + "/wall_e/temp"
 
-@bp.route('/results/browse/', defaults={'req_path': ''})
+    r = requests.get(DL_URL)
+    current_app.logger.debug(f"Response headers {r.headers}")
+    if r.headers["Content-Type"] != 'application/zip':
+        current_app.logger.info(f"Response code {r.status_code}, url {r.url}")
+        current_app.logger.info(f"couldn't download {DL_URL}. Got headers {r.headers}.")
+        return abort(404)
+
+    filenamezip = r.headers['Content-Disposition'].split("=")[-1].strip('"')
+    filepathzip = f"{TEMP_DIR}/{filenamezip}"
+    dirname = filenamezip[:-4]
+
+    with open(filepathzip,'wb') as output_file:
+        output_file.write(r.content)
+    current_app.logger.debug(f"Done downloading to {filepathzip}")
+
+    shutil.unpack_archive(filepathzip, f"{TEMP_DIR}/{dirname}", format="zip")
+    current_app.logger.debug(f"Unpacked zip to {TEMP_DIR}/{dirname}")
+    os.remove(filepathzip)
+
+    return redirect(f"/results/browse/{dirname}")
+
+
+
 @bp.route('/results/browse/<path:req_path>')
 def browse_files(req_path):
     """
     Displays a file tree
     """
-    ROOT_DIR = APP_BASE_PATH # Where the base dir is. Change this value.
+    TEMP_DIR = APP_BASE_PATH + "/wall_e/temp"
     file_content = None
 
-    # Finds the previus folder
-    previus_directory = "/".join(req_path.split('/')[:-1])
-    abs_path = os.path.join(ROOT_DIR, req_path)
+    # Finds the previous folder
+    previous_directory = "/".join(req_path.split('/')[:-1])
+    abs_path = os.path.join(TEMP_DIR, req_path)
 
     link_content = "/".join(request.path.split('/')[3:])
 
@@ -66,7 +98,7 @@ def browse_files(req_path):
         file_extention = abs_path.split('.')[-1]
 
         return render_template(
-            'browse.html', previus_directory=previus_directory,
+            'browse.html', previous_directory=previous_directory,
             file_type=file_extention, file_content=file_content,
             link_content=link_content)
 
@@ -74,4 +106,4 @@ def browse_files(req_path):
     # Show directory contents
     files = os.listdir(abs_path)
     return render_template(
-        'browse.html', files=files, previus_directory=previus_directory, link_content=link_content)
+        'browse.html', files=files, previous_directory=previous_directory, link_content=link_content)
