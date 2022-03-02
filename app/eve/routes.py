e4e703f27980000a3343ae8ee6e8ae2290b339e3
Andreas Arnesson <Andreas_osby@hotmail.com>
2022-03-02 11:07:14 +0100

    fixes

--- app/eve/routes.py
+++ app/eve/routes.py
@@ -23,30 +23,30 @@ def before_request():
 
 
 
-@bp.route('/eve/reset', methods=['GET', 'POST'])
-def reset():
-    """
-    Temporary route to reset database and load a test assignment.
-    """
-    course_id, course_name = 2508, 'python'
+# @bp.route('/eve/reset', methods=['GET', 'POST'])
+# def reset():
+#     """
+#     Temporary route to reset database and load a test assignment.
+#     """
+#     course_id, course_name = 2508, 'python'
 
-    Course.query.filter(Course.id > 0).delete()
-    c1 = Course(id=course_id, name=course_name, active=1)
-    c2 = Course(id=123, name='ike-aktiv', active=0)
-    db.session.add(c1)
-    db.session.add(c2)
-    db.session.commit()
+#     Course.query.filter(Course.id > 0).delete()
+#     c1 = Course(id=course_id, name=course_name, active=1)
+#     c2 = Course(id=123, name='ike-aktiv', active=0)
+#     db.session.add(c1)
+#     db.session.add(c2)
+#     db.session.commit()
 
-    Submission.query.filter(Submission.id > 0).delete()
-    db.session.commit()
+#     Submission.query.filter(Submission.id > 0).delete()
+#     db.session.commit()
 
-    User.query.filter(User.id > 0).delete()
-    user = User(username='dbwebb')
-    user.password = 'super-secret'
-    db.session.add(user)
-    db.session.commit()
+#     User.query.filter(User.id > 0).delete()
+#     user = User(username='dbwebb')
+#     user.password = 'super-secret'
+#     db.session.add(user)
+#     db.session.commit()
 
-    return {"message": "Submission and Course table has been reset with dummy data"}
+#     return {"message": "Submission and Course table has been reset with dummy data"}
 
 
 
