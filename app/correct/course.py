63849334673272c7cbd58ae48bfb73fdf2984cc5
AndreasArne <andreas_osby@hotmail.com>
2022-12-26 23:32:09 +0100

    i think i have a structure to work on

---
+++ app/correct/course.py
@@ -0,0 +1,113 @@
+# from app.exceptions.exceptions import MissingGroupError
+# from flask import current_app
+
+from app.settings import Config
+
+def course(canvas, course_id, course_name,):
+    course = canvas.get_course(course_id)
+    groups = course.get_groups(include=["users"])
+    users = course.get_users()
+    assignments = course.get_assignments()
+    submissions = course.get_multiple_submissions(
+        student_ids="all",
+        workflow_state="submitted",
+        include=["assignment"]
+    )
+    config = Config(course_name)
+
+
+    users_to_skip = {}
+    for sub in submissions:
+        assignment = get_assignment(assignments, sub.assignment_id)
+        if should_grade(sub, users_to_skip, assignment, config):
+            # grade
+            add_users_to_skip_from_group(sub.user_id, groups, assignment, users_to_skip)
+            print("graded", sub)
+        else:
+            print("Skipped", sub)
+
+
+
+def should_grade(sub, users_to_skip, assignment, config):
+    """
+    should skip if graded group partner if group assignment
+    and if setting says it should skip assignment
+    """
+    skip = (
+        not assignment.grade_group_students_individually 
+        and sub.user_id in users_to_skip.get(assignment.id, [])
+    ) or assignment.name in config["ignore_assignments"]
+
+    return not skip
+
+
+
+
+def add_users_to_skip_from_group(user_id, groups, assignment, users_to_skip):
+    group = get_group_with_category_and_user(
+        groups,
+        assignment.group_category_id,
+        user_id
+    )
+    partners = get_users_group_partners(group, user_id)
+    if assignment.id in users_to_skip:
+        users_to_skip[assignment.id].extend(partners)
+    else:
+        users_to_skip[assignment.id] = partners
+
+
+
+def grade_submission(submission, users):
+    pass
+
+
+
+def get_group_partners_submissions(partners, submissions, submissions_to_skip):
+    for sub in submissions:
+        for user in partners:
+            if sub.user_id == user["id"]:
+                submissions_to_skip.append(sub)
+    return submissions_to_skip
+
+
+def grade_submissions_group():
+    pass
+
+
+
+def get_users_group_partners(group, user_id):
+    """
+    Return a list with a groups users without a user
+    """
+    return [user["id"] for user in group.users if user["id"] != user_id]
+
+
+
+def get_assignment(assignments, assignment_id):
+    for assignment in assignments:
+        if assignment.id == assignment_id:
+            return assignment
+
+
+
+def get_group_with_category_and_user(groups, group_category_id, user_id):
+    for group in groups:
+        if group.group_category_id == group_category_id:
+            if user_id in [user["id"] for user in group.users]:
+                return group
+    # raise MissingGroupError("No group for group_category_id {group_category_id} and user_id {user_id} found")
+
+# hitta användares grupp
+# sparas deras id
+# vid ny rättning
+# jämför mot id
+
+if __name__ == "__main__":
+    from canvasapi import Canvas
+
+    canvas = Canvas(
+        URL,
+        TOKEN
+    )
+
+    course(canvas, CID, CN)
--- app/new/routes.py
+++ app/new/routes.py
@@ -8,7 +8,8 @@ from app.models import Submission, Course, format_dict
 import app.globals as g
 
 from canvasapi import Canvas
-from app.runner import run
+from canvasapi.exceptions  import ResourceDoesNotExist
+from app.correct import course as correct_course
 
 @bp.route('/new/grade', methods=['GET'])
 @auth.requires_authorization_header
@@ -24,41 +25,12 @@ def fetch():
 
     for c in active_courses:
 
-        run.grade_course(canvas, c.id, c.name)
-        # get submision
+        try:
+            correct_course.course(canvas, c.id, c.name)
+        except ResourceDoesNotExist as e:
+            # do something here to notify me that course is missing
+            raise e
 
-
-        students = canvas.users_and_acronyms()
-        subs = canvas.get_gradeable_submissions()
-
-        for sub in subs:
-            assignment_id = sub["assignment_id"]
-            user_id = sub["user_id"]
-
-            # Ignores the submission if it exists
-            stud_attempts = Submission.query.filter_by(
-                assignment_id=assignment_id, user_id=user_id)
-            exists = [a for a in stud_attempts if a.workflow_state in ['new', 'tested']]
-
-            if exists:
-                continue
-            try:
-                user_acronym = students[user_id]
-            except KeyError:
-                current_app.logger.info(
-                    f"User id {user_id} from submission {sub['id']} is not among students fetched from Canvas."
-                )
-                continue
-            assignment_name = canvas.get_assignment_name_by_id(assignment_id=assignment_id)
-
-            s = Submission(
-                assignment_id=assignment_id, assignment_name=assignment_name, user_id=user_id,
-                user_acronym=user_acronym, course_id=c.id, attempt_nr=sub["attempt"])
-            current_app.logger.info(f"Found submission for {user_acronym} in assignment {assignment_name}.")
-
-
-            db.session.add(s)
-            db.session.commit()
 
     return { "message": "Successfully fetched new assignments from canvas" }, 201
 
